825d87feec4364ce391e21031cdc7a4c673a6f6c
iamminhquan <minhquan542005@gmail.com>
2025-10-15 21:51:40 +0700

    chore: initial project ASP.NET Core MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BaiTapNhom.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
