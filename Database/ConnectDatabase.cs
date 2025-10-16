9819c5680d2d480c766fc8f30096800882d67967
immqq <0306231328@caothang.edu.vn>
2025-10-16 12:55:49 +0700

    feat: connect to database by SqlConnection object

---
+++ Database/ConnectDatabase.cs
@@ -0,0 +1,12 @@
+﻿namespace BaiTapNhom.Database
+{
+    public class ConnectDatabase(string connectionString)
+    {
+        private readonly string _connectionString = connectionString;
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
